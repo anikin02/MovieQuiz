86a79d2a9425f2a24839b23a66d3c2034b740882
anikin02 <danilanikin02@icloud.com>
2026-09-14 13:06:33 +1000

    Пройден спринт 4

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,12 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Данил on 14/09/2026.
+//
+
+struct QuizQuestion {
+  let image: String
+  let text: String
+  let correctAnswer: Bool
+}
---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Данил on 14/09/2026.
+//
+
+struct QuizResultsViewModel {
+  let title: String
+  let text: String
+  let buttonText: String
+}
